e940ac90e4b65768be6b9cd3d9bb85b39782b3da
xxljunjun <793117268@qq.com>
2021-11-30 18:55:35 +0800

    配置了热更新的端口号8080

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');  //关联js和html
 module.exports = {
+  mode: 'development',
+  //Using source maps
+  // devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true
+  },
   //项目入口
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
+    another: './src/another-module.js',
   },
   //项目出口
   output: {
@@ -12,9 +19,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  //插件
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
   ],
 };
